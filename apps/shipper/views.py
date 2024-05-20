d95d7b00853a4dabc10b5e993a1794569c9dec62
Sulaimanovuran <sulaimanovuran@gmail.com>
2024-05-20 23:27:06 +0600

    add try/except

--- apps/shipper/views.py
+++ apps/shipper/views.py
@@ -16,32 +16,31 @@ def hello():
 def get_data():
 
     if request.method == "POST":
-        # try:
-        # Получаем chat_id чтобы транслировать ошибки если они есть
-        # chat_id = request.data.get('chat_id')
-        print(request.data)
-        week = request.get_json()['week']
-    
-        # Отправляем запрос на получени данных из API Shipper
-        data: dict = get_list()
-
-        # При успешном запросе создаем новые и обновляем старые данные в БД
-        if "ERROR" not in data:
-            packages: list[Shipper] = PackagesDB(data, week)
-        else:
-            return {"status": "error", "message": data}
+        try:
+            # Получаем chat_id чтобы транслировать ошибки если они есть
+            # chat_id = request.data.get('chat_id')
+            print(request.data)
+            week = request.get_json()['week']
         
-        # Проверяем создан ли объект PackagesDB (создание и обновление данных о посылках в БД)
-        if isinstance(packages, PackagesDB):
+            # Отправляем запрос на получени данных из API Shipper
+            data: dict = get_list()
 
-            # Проверяем что нет ошибок при получении данных для целевой недели
-            if 'ERROR' not in packages.weekly_packages:
-                message = telegram_message_info(packages.weekly_packages, week)
-
-                return {"status":"success", "message": message}
+            # При успешном запросе создаем новые и обновляем старые данные в БД
+            if "ERROR" not in data:
+                packages: list[Shipper] = PackagesDB(data, week)
             else:
-                return {"status": "error", "message": packages.weekly_packages}
-        # except Exception as ex:
-        #     print(ex)
-        #     return {"status": "error", "message": f"Ошибка:\n{ex}"}
+                return {"status": "error", "message": data}
+            
+            # Проверяем создан ли объект PackagesDB (создание и обновление данных о посылках в БД)
+            if isinstance(packages, PackagesDB):
+
+                # Проверяем что нет ошибок при получении данных для целевой недели
+                if 'ERROR' not in packages.weekly_packages:
+                    message = telegram_message_info(packages.weekly_packages, week)
+
+                    return {"status":"success", "message": message}
+                else:
+                    return {"status": "error", "message": packages.weekly_packages}
+        except Exception as ex:
+            return {"status": "error", "message": f"Ошибка:\n{ex}"}
             
